6673ef0403bb2204f1d69fceb5810bb94c2891f4
jojostx <ikuskid7@gmail.com>
2025-04-08 10:36:52 +0100

    External Lab: Create a Rust library :: clean up

--- cli-utils/target/debug/deps/cli_utils-25a5ad813763a555.d
+++
@@ -1,9 +0,0 @@
-/mnt/c/Users/L14/Desktop/code/rust-coursera-projects/cli-utils/target/debug/deps/libcli_utils-25a5ad813763a555.rmeta: src/lib.rs Cargo.toml
-
-/mnt/c/Users/L14/Desktop/code/rust-coursera-projects/cli-utils/target/debug/deps/cli_utils-25a5ad813763a555.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- cli-utils/target/debug/deps/cli_utils-b1146d2ed962d5c7.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/L14/Desktop/code/rust-coursera-projects/cli-utils/target/debug/deps/cli_utils-b1146d2ed962d5c7: src/lib.rs
-
-/mnt/c/Users/L14/Desktop/code/rust-coursera-projects/cli-utils/target/debug/deps/cli_utils-b1146d2ed962d5c7.d: src/lib.rs
-
-src/lib.rs:
