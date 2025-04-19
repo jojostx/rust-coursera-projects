8a91291e1841a3a7c3478c277fdbf40cc883f8a1
jojostx <ikuskid7@gmail.com>
2025-04-19 09:45:15 +0100

    CRS-2 :: Data Engineering with Rust - module 1 - Data Engineering with Rust Sandbox :: v4)

--- benchmark_collection/src/main.rs
+++ benchmark_collection/src/main.rs
@@ -1,67 +1,38 @@
-use std::{
-    collections::{BTreeMap, HashMap, LinkedList},
-    thread::{self, JoinHandle},
-    time::Instant,
-};
+use std::collections::{BTreeMap, HashMap, LinkedList};
+use std::time::Instant;
+
+fn benchmark<T>(name: &str, collection: T)
+where
+    T: Fn(),
+{
+    let start = Instant::now();
+    collection();
+    let duration = start.elapsed();
+    println!("Time taken for {}: {:?}", name, duration);
+}
 
 fn main() {
-    let start = Instant::now();
-
-    // spawn multiple threads for insertions into collection types
-    let mut handles: Vec<JoinHandle<()>> = Vec::new();
-
-    let handle1 = thread::spawn(|| {
-        let start = Instant::now();
+    benchmark("Vector", || {
         let mut nums = vec![];
-        (1..=10000).for_each(|x| {
-            nums.push(x);
-        });
-        let duration = start.elapsed();
-        println!("Time taken for Vector: {:?}", duration);
+        (1..=100000).for_each(|x| nums.push(x));
     });
 
-    // LinkedList
-    let handle2 = thread::spawn(|| {
-        let start = Instant::now();
+    benchmark("LinkedList", || {
         let mut nums = LinkedList::new();
-        (1..=10000).for_each(|x| {
-            nums.push_back(x);
-        });
-        let duration = start.elapsed();
-        println!("Time taken for LinkedList: {:?}", duration);
+        (1..=100000).for_each(|x| nums.push_back(x));
     });
 
-    // HashMap
-    let handle3 = thread::spawn(|| {
-        let start = Instant::now();
+    benchmark("HashMap", || {
         let mut nums = HashMap::new();
-        (1..=10000).for_each(|x| {
+        (1..=100000).for_each(|x| {
             nums.insert(x, 0);
         });
-        let duration = start.elapsed();
-        println!("Time taken for HashMap: {:?}", duration);
     });
 
-    // BTreeMap
-    let handle4 = thread::spawn(|| {
-        let start = Instant::now();
+    benchmark("BTreeMap", || {
         let mut nums = BTreeMap::new();
-        (1..=10000).for_each(|x| {
+        (1..=100000).for_each(|x| {
             nums.insert(x, 0);
         });
-        let duration = start.elapsed();
-        println!("Time taken for BTreeMap: {:?}", duration);
     });
-
-    handles.push(handle1);
-    handles.push(handle2);
-    handles.push(handle3);
-    handles.push(handle4);
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    let duration = start.elapsed();
-    println!("Time taken: {:?}", duration);
 }
