8a35e440f837bb1f67ccd5f33ff28ca414cd608a
jojostx <ikuskid7@gmail.com>
2025-05-06 18:43:57 +0100

    catsay v2

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,13 +1,52 @@
-fn main() {
-    let message = std::env::args()
-        .nth(1)
-        .expect("Missing the message. Usage: catsay <message>");
+use clap::Parser;
+use colored::Colorize;
 
-    println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    println!("   \\");
-    println!("    /\\___/\\");
-    println!("   ( o   o )");
-    println!("   =(  I  )=");
+#[derive(Parser)]
+struct Options {
+    #[clap(default_value = "Meow!")]
+    /// What does the cat say?
+    message: String,
+
+    #[clap(short = 'd', long = "dead")]
+    dead: bool,
+
+    #[clap(short = 'f', long = "file")]
+    catfile: Option<std::path::PathBuf>,
+}
+
+fn main() {
+    let options = Options::parse();
+    let mut message = options.message;
+
+    let eye = if options.dead { "x" } else { "o" };
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog.");
+        message = "Meow".to_string();
+    }
+
+    let catfile = options.catfile;
+
+    match &catfile {
+        Some(path) => {
+            // read from file
+            let msg = &format!("could not read file {:?}", path);
+            let cat_picture = std::fs::read_to_string(path).expect(msg);
+
+            let eye = format!("{}", eye.red().bold());
+
+            let cat_picture = cat_picture.replace("{eye}", &eye);
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!("{}", cat_picture);
+        }
+        None => {
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!(" \\");
+            println!("  \\");
+            println!("   \\");
+            println!("    /\\___/\\");
+            println!("   ( {}   {} )", eye.red().bold(), eye.red().bold());
+            println!("   =(  I  )=");
+        }
+    }
 }
